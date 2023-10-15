3eda791a2c4efd63ccb4b1000274e7edcea7071c
Stan Cosmin <94742249+StanCosmin28@users.noreply.github.com>
2023-10-15 20:08:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+/* GSAP effect for the cup */
+gsap.fromTo("#coffeeapp", { x: 1000 }, { x: 0, duration: 1, delay: 0.2 });
+gsap.fromTo("#titleapp", { x: -1000 }, { x: 0, duration: 1, delay: 0.2 }), "<";
+
+gsap.fromTo(
+  "#coffeeapp",
+  { y: -30 },
+  { y: 10, duration: 2, yoyo: true, repeat: -1 }
+),
+  "<";
+gsap.fromTo(
+  "#coffeeapp",
+  { rotate: "-5deg" },
+  {
+    rotate: "5deg",
+    duration: 1,
+    yoyo: true,
+    repeat: -1,
+  }
+),
+  "<";
+gsap.fromTo("#ab", { y: -1000 }, { y: 0, duration: 1.5 }, "<");
+
+gsap.fromTo("#le", { y: -1000 }, { y: 0, duration: 1.5, delay: 0.5 }, "<");
+
+gsap.fromTo("#co", { y: -1000 }, { y: 0, duration: 1.5, delay: 0.5 }, "<");
+
+gsap.fromTo(
+  "#flwapp",
+  { rotate: "20deg" },
+  {
+    rotate: "-10deg",
+    duration: 1,
+    yoyo: true,
+    repeat: -1,
+    delay: 0.3,
+  }
+);
